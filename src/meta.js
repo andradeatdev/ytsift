e66428fa0e586d985098997c0890113e959cd0b3
andradeatdev <gabrielandrade@proton.me>
2026-05-23 19:45:19 -0300

    chore(release): bump version to 1.0.1

--- src/meta.js
+++ src/meta.js
@@ -5,7 +5,7 @@
 // @match               https://www.youtube.com/*
 // @grant               GM_addStyle
 // @noframes
-// @version             1.0.0
+// @version             1.0.1
 // @author              hdyzen
 // @description         Lightweight YouTube channel filter for titles, duration, views, age and watch state.
 // @description:pt-BR   Filtro leve para canais do YouTube por título, duração, visualizações, idade e status de visualização.
