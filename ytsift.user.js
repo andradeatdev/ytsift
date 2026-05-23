9342ba649a04071241ac3fb9f5853541f923021d
andradeatdev <gabrielandrade@proton.me>
2026-05-23 19:43:13 -0300

    chore(metadata): improve userscript metadata translations and wording

--- ytsift.user.js
+++ ytsift.user.js
@@ -1,13 +1,15 @@
 // ==UserScript==
-// @name         YouTube - ytsift
-// @namespace    https://greasyfork.org/users/821661
-// @match        https://www.youtube.com/*
-// @grant        GM_addStyle
+// @name               ytsift - Lightweight YouTube channel filter
+// @name:pt-BR         ytsift - Filtro de vídeos em canais do YouTube
+// @namespace          https://greasyfork.org/users/821661
+// @match              https://www.youtube.com/*
+// @grant              GM_addStyle
 // @noframes
-// @version      1.0.0
-// @author       hdyzen
-// @description  Intelligent local filter for YouTube channel videos
-// @license      GPL-3.0
+// @version            1.0.0
+// @author             hdyzen
+// @description        Lightweight YouTube channel filter for titles, duration, views, age and watch state.
+// @description:pt-BR  Filtro leve para canais do YouTube por título, duração, visualizações, idade e status de visualização.
+// @license            GPL-3.0
 // ==/UserScript==
 
 const CONFIG = {
